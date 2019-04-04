c99509d5487355d813344c93ab2c980f563f10b6
Dmitrii <sayottd@gmail.com>
2019-04-05 04:09:09 +1100

    Выполнено ДЗ № 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,44 +1,75 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MainClass {
+    static int[] mass = new int[]{1, 0, 0, 0, 1, 0, 1, 1, 0, 1};
+    static int[] mass2 = new int[8];
+    static int[] mass3 = new int[] {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+    static int[] [] mass4 = new int[5] [5];
+    static int[] mass5 = new int []{3,2,5,4,9};
+
     public static void main(String[] args) {
-        System.out.println (mathExpression( 2,2,4,5 ));
-        System.out.println (checkingNumLimits( 11,9 ));
-        positiveNumCheck( -1 );
-        System.out.println (negativeNumCheck( 10 ));
-        hello( "Эржан" );
-        leapYearCheck(2019);
-    }
+        changingMass();
+        System.out.println(Arrays.toString(mass));
 
-    char c = 35;
-    byte b = 100;
-    short s = 30000;
-    int i = 500;
-    long l = 5000000000L;
-    float f = 10000000000.5F;
-    double d = 30000000232.412F;
-    boolean boo = true;
-    String str = "SomeText";
+        fillMass( );
+        System.out.println(Arrays.toString( mass2 ));
 
-   private static int mathExpression (int a, int b, int c, int d){  //метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат
-       return a * (b + (c / d));
-    }
-    private static boolean checkingNumLimits (int a, int b){   // метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах от 10 до 20(включительно)
-       return (a + b >= 10 && a + b <= 20);
-    }
-    private static void positiveNumCheck (int a){             // метод который проверяет положительное ли число ему передали
-        System.out.println( a>=0 ? "Число положительное" : "Число отрицательное");
-    }
-    private static boolean negativeNumCheck (int a){            //метод который проверяет отрицательное число ему передали
-        return (a < 0);
-    }
-    private static void hello (String name){
-        System.out.println("Привет, " + name+ "!");
-    }
-    private static void leapYearCheck (int year){               //метод, который определяет является ли год високосным
-        if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)){
-            System.out.println(year + " год високосный");
+        multiplicationMass(  );
+        System.out.println(Arrays.toString( mass3 ));
+
+        diagonalFill();
+
+        minmaxelements(mass5);
+
+
         }
-        else {
-            System.out.println(year + " год не является високосным");
+
+
+
+
+    private static void changingMass() {              //заменяет число 1 на 0, а на 0 на 1 в массиве
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] == 1) mass[i] = 0;
+            else mass[i] = 1;
         }
     }
-}
+
+    private static void fillMass(){                  //заполняет массив значениями 0 3 6 9 12 15 18 21
+        for (int i = 0, j = 0; i <mass2.length; i++ , j+=3){
+            mass2[i]=j;
+
+        }
+    }
+
+    private static void multiplicationMass (){     //числа меньшие 6 в массиве умножить на 2
+            for (int i= 0; i<mass3.length; i++){
+                if (mass3[i]<6){
+                    mass3[i]=mass3[i]*2;
+                }
+            }
+    }
+    private static void diagonalFill (){                         //заполнение  диагональные элементы массива единицами
+        for (int i = 0; i < mass4.length; i++) {
+            for (int j = 0, j2 = mass4[i].length; j < mass4[i].length; j++, j2--) {
+                if (i == j || i == (j2 - 1)) {
+                    mass4[i][j] = 1;
+                }
+                System.out.print(mass4[i][j] + "");
+            }
+            System.out.println("");
+        }
+    }
+
+    private static void minmaxelements (int m[]){    //выводит минимальное и максимальное число в массиве
+        int max = mass5[0];
+        int min = mass5[0];
+        for (int x : mass5){
+            if (x>max) max=x;
+            if (x<min) min=x;
+        }
+        System.out.println(min + " " + max);
+    }
+
+            }
+
